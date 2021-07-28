c015777a9ee3517e5d6fd30cc3a7aacdaf333e0c
Seogeurim <geulims@naver.com>
2021-07-29 03:44:23 +0900

    add style loader and style sample code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import './styles';
+
 const $app = document.querySelector('#app');
 
-console.log($app);
+$app.innerHTML = `
+  <div class="welcome">
+    <h1 class="welcome__title">Webpack es6 Boilerplate</h1>
+    <p class="welcome__desc">This is JavaScript boilerplate using webpack v5.</p>
+  </div>
+`;
