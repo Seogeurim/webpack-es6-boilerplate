3f3f01d58684e20a61b9a614b0c05736a35ccf22
Seogeurim <geulims@naver.com>
2021-07-29 03:59:13 +0900

    add eslint & prettier
    - add eslint of airbnb-base
    - add prettier

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles';
+import './styles/index.scss';
 import icon from './assets/code.png';
 
 const $app = document.querySelector('#app');
@@ -8,7 +8,7 @@ $app.innerHTML = `
     <h1 class="welcome__title">Webpack es6 Boilerplate</h1>
     <p class="welcome__desc">
       <img src="${icon}" alt="icon" />
-      This is JavaScript boilerplate using webpack v5.
+      This is a JavaScript boilerplate using webpack v5.
     </p>
   </div>
 `;
