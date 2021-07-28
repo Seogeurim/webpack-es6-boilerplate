83415f09c6e0c0bd17f103cc14f847baf87bc9e7
Seogeurim <geulims@naver.com>
2021-07-29 03:50:19 +0900

    add file loader and sample image

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './styles';
+import icon from './assets/code.png';
 
 const $app = document.querySelector('#app');
 
 $app.innerHTML = `
   <div class="welcome">
     <h1 class="welcome__title">Webpack es6 Boilerplate</h1>
-    <p class="welcome__desc">This is JavaScript boilerplate using webpack v5.</p>
+    <p class="welcome__desc">
+      <img src="${icon}" alt="icon" />
+      This is JavaScript boilerplate using webpack v5.
+    </p>
   </div>
 `;
